fbe5be705769c9917dee1b5f41bffe0dc7122571
RISHII7 <rishikesh.palande07@gmail.com>
2024-08-21 11:35:09 +0530

    Basics of Javascript

---
+++ 01. Beginners/01. Basics/console.js
@@ -0,0 +1,29 @@
+// What is Javascript?
+// Javascript is a versatile and widely used programming language that plays a crucial role in web development.
+// It enables developers to create interactive and dynamic elements within web pages, enhancing the user experience by add functionality that responds to user actions.
+// Javascript is primarily executed in web browsers but it's used in other environments like server-side scripting (Node.js)
+
+// ---------- CONSOLE ----------
+
+console.log('Hello World');
+// log
+// clg
+// console.log(console); // console object
+
+console.error('This is an error');
+console.warn('This is an warning');
+console.table({ name: 'Rishikesh', age: '23', location: 'India', location: ['Maharashtra', 'Pune'] });
+
+// ---------- COMMENTS ----------
+
+// This is one line comment
+/*
+    This is 
+    multi-line 
+        comment
+*/
+/** Unordered Comment
+ * bla
+ * bla
+ * bla
+ */
