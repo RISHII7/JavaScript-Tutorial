6bb2a9b25838e8decca3af73673f4ffbeaa9f7be
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-04 22:46:13 +0530

    Arrays

---
+++ 01. Beginners/04. Reference Type (Basics)/01. Arrays/Exercise/app.js
@@ -0,0 +1,18 @@
+// 1. Create array name (favSingers) Store 3 favorite singers.
+// 2. log the first singer in that array.
+// 3. Create array name (favNumbers) & store 4 fav Numbers.
+// 4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
+// 5. Now Access each item in that array by using [] notation.
+
+let favSinger = ["Raga", "Seedhe Maut", "Krsna"];
+console.log(favSinger[0]);
+
+let favNumbers = [2, 4, 5, 7];
+
+let mixedArr = ["string", [1, 2, 3], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[1][1]);
+console.log(mixedArr[1][2]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
