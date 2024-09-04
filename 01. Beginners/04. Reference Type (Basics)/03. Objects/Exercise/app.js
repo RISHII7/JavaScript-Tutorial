7a6fb2258d07ee23a8b494f130ee5177f7065ec1
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-04 23:03:37 +0530

    Objects

---
+++ 01. Beginners/04. Reference Type (Basics)/03. Objects/Exercise/app.js
@@ -0,0 +1,17 @@
+// 1. Create object name (car)
+// 2. add (type, model, color) as properties
+// 3. check the type of object
+// 4. Update the type property to "Toyota"
+// 5. Add new property wheels
+// 6. log car to the console.
+
+let car = {
+    type: "Ford",
+    model: "Mustang",
+    color: "Red"
+};
+
+console.log(typeof(car));
+car.type = "Toyota";
+car.wheels = 4;
+console.log(car)
