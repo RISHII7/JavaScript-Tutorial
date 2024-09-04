1a3082c1e936384fda0b750e5aa31133386e22a0
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-04 21:22:04 +0530

    While Loop

--- 01. Beginners/03. Control Flow/03. For Loop/Live Coding/app.js
+++ 01. Beginners/03. Control Flow/03. For Loop/Live Coding/app.js
@@ -4,7 +4,10 @@ for (let i = 0; i <= 5; i++) {
     console.log("Hello World", i);
 }
 
-// EXERCISE
-// ... (let ; = ; < ; ) {
-//   console.log(i);
-// }
+
+for (let i = 0; i < 3; i++) {
+    console.log('Outer Loop', i);
+    for (let j = 0; j < 5; j++) {
+        console.log('Inner Loop', j);
+    }
+}
