ca33f8daecadaac9348ab08f4e0f3303628dd1c8
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-05 20:58:04 +0530

    Rest Operator

---
+++ 02. Intermediate/06. Rest Operator/Exercise/app.js
@@ -0,0 +1,16 @@
+// 1. Create a function name (sum) which takes (unlimited) amount of parameters
+// 2. No matter how many parameters you provide it will return the sum value of those params
+
+// const sum = (...num) => {
+//     return num.reduce((prev, curr) => {
+//         return prev + curr
+//     });
+// }
+
+// console.log(sum(1, 2, 3, 4, 5, 67));
+
+const sum = (a, ...b) => {
+    return a + b;
+}
+
+console.log(sum(1, 2, 3, 4));
