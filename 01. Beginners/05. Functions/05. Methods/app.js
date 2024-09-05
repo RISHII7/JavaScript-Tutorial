b684d1cb5e788327834b6931837157ba330b1e4e
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-05 11:51:21 +0530

    Methods

---
+++ 01. Beginners/05. Functions/05. Methods/app.js
@@ -0,0 +1,27 @@
+// a method is a function that is associated with an object. It allows objects to perform actions or provide functionalities.
+
+// ------------------------------------
+// Defining a method outside the object
+// function greet() {
+//   return `Hello, my name is ${person.name} and I am ${person.age} years old.`;
+// }
+
+// const person = {
+//   name: "John",
+//   age: 30,
+//   greet,
+// };
+
+// console.log(person.greet());
+
+// ------------------------------------
+
+const person = {
+    name: "John",
+    age: 30,
+    greet: function() {
+        return `Hello, my name is ${person.name} and I'am ${person.age} years old.`
+    },
+};
+
+console.log(person.greet());
