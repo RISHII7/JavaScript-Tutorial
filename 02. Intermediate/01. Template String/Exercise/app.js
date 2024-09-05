9522d73da38b8145b31866ac65c40831bb6d31ba
RISHII7 <rishikesh.palande07@gmail.com>
2024-09-05 12:13:10 +0530

    Template Strings/Literals

---
+++ 02. Intermediate/01. Template String/Exercise/app.js
@@ -0,0 +1,18 @@
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variables (firstName, lastName)
+// 3. Use that variables in template string.
+
+let str = `The quick
+brown fox
+jumps over
+the lazy dog`
+console.log(str);
+
+let firstName = 'Rishikesh';
+let lastName = 'Palande';
+console.log(`My full name is ${firstName} ${lastName}`);
